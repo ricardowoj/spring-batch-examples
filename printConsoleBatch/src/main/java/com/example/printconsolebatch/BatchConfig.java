15bbe9defe5c79576746b78a04b9dcd1b4032d6d
Ricardo <ricardowoj@hotmail.com>
2023-11-12 21:20:41 +0000

    add parameter in tasklet.

--- printConsoleBatch/src/main/java/com/example/printconsolebatch/BatchConfig.java
+++ printConsoleBatch/src/main/java/com/example/printconsolebatch/BatchConfig.java
@@ -5,7 +5,11 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,17 +26,24 @@ public class BatchConfig {
     @Bean
     public Job printHelloSpringBatchJob() {
         return jobBuilderFactory
-                .get("printHelloJob")
+                .get("printHelloStep")
                 .start(printHelloStep())
+                .incrementer(new RunIdIncrementer())
                 .build();
     }
     
     public Step printHelloStep() {
         return stepBuilderFactory
                 .get("printHelloStep")
-                .tasklet((stepContribution, chunkContext) -> {
-                    System.out.println("Hello Spring Batch!!!");
-                    return null;
-                }).build();
+                .tasklet(getTasklet()).build();
+    }
+
+    @Bean
+    @StepScope
+    public static Tasklet getTasklet() {
+        return (stepContribution, chunkContext) -> {
+            System.out.println(String.format("Hello %s!!!", System.getProperty("name")));
+            return null;
+        };
     }
 }
