214c12847f1e908301a63d486e150a4b4418e251
Ricardo <ricardowoj@hotmail.com>
2023-11-12 21:54:26 +0000

    add example printConsoleNumber.

---
+++ printConsoleNumber/src/test/java/com/example/printconsolenumber/PrintConsoleNumberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.printconsolenumber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrintConsoleNumberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
